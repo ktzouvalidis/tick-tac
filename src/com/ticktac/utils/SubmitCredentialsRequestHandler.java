06e060ce6900bac4fcab1b22cade3aa868036d3b
unknown <nnystad@gmail.com>
2017-12-03 22:12:46 +0100

    requesthandler for cardcredentials include cardNumber

--- src/com/ticktac/utils/SubmitCredentialsRequestHandler.java
+++ src/com/ticktac/utils/SubmitCredentialsRequestHandler.java
@@ -12,13 +12,21 @@ import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 
+import com.ticktac.business.CardCredentials;
+
 public class SubmitCredentialsRequestHandler implements RequestHandler {
 
 	@Override
 	public String handleRequest(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		CardCredentials cardCredentials = new CardCredentials();
+		cardCredentials.setCardNumber(Integer.parseInt(request.getParameter("cardNumber")));
+		//cardCredentials.setCv2Number(Integer.parseInt(request.getParameter("cv2Number")));
+		
+		
 		Client client = ClientBuilder.newClient();
-		WebTarget webResource = client.target("http://localhost:8081").path("banking");
+		WebTarget webResource = client.target("http://localhost:8081").path("banking")
+				.queryParam("cardNumber", cardCredentials.getCardNumber());
 		
 		String out = webResource.request().accept("applicatio/json").get(String.class);
 		
